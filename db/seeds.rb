f5c46f3b10cba048b190b18f7539e73922f0f996
Gordon <gordon.wallac3@yahoo.com>
2017-08-07 21:19:53 -0600

    added radio buttons for status feild for blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ puts "3 tipoc creation"
 
 10.times do |blog|
 	Blog.create!(
+		topic_id: Topic.last.id,
 		title: "taco nacho amego #{blog}",
 		body: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untraveled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided."
 	)
