159c6d238301c220b9b796806da720bf2dd9d831
Gordon <gordon.wallac3@yahoo.com>
2017-08-20 15:55:56 -0600

    added favicons to all pages and removed topic id call for selected in blog form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -44,10 +44,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'WOOT! You made a blog!' }
-        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
